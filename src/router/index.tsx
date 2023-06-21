1f7ae310347a3c6d89fe123611c1b2a890ff40d4
owo <owocc@qq.com>
2023-06-21 10:18:06 +0800

    添加路由,让页面更优雅~哈哈哈哈哈

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,1 +1,21 @@
 // 创建路由
+import { createBrowserRouter } from "react-router-dom";
+import DefaultLayout from "../layout/default";
+import { lazy } from "react";
+const GuessNumber = lazy(() => import("../pages/guess-number/GuessNumberPage"));
+
+// 路由信息
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "guess-number",
+        element: <GuessNumber />,
+      },
+    ],
+  },
+]);
+
+export default router;
